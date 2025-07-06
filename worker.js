09deed29e9a94466f5b8a1b2579d3691185bf0e6
Silso <dev@sil.so>
2025-07-06 10:21:16 +0200

    add sitemap and cleanup worker.js

--- worker.js
+++ worker.js
@@ -1,13 +1,5 @@
 export default {
   async fetch(request, env, ctx) {
-    const url = new URL(request.url);
-    
-    // Redirect www → non-www
-    if (url.hostname === "www.sil.so") {
-      url.hostname = "sil.so";
-      return Response.redirect(url.toString(), 301);
-    }
-
     return env.ASSETS.fetch(request);
   }
 }
