04501e30731a90bb27171004382d36246c1b4056
Silso <dev@sil.so>
2025-12-05 14:48:41 +0100

    Inject BlogNavFixScript into blog post response
    Added BlogNavFixScript to the response pipeline for blog posts by attaching it to the 'body' event. This likely addresses navigation or UI issues in blog post rendering.

--- worker.js
+++ worker.js
@@ -223,7 +223,6 @@ async function handleBlogPost(slug, request, env) {
       .on("img[src]", new AssetPathHandler("src"))
       .on("img[srcset]", new SrcSetHandler())
       .on("a[href]", new LinkHandler())
-      .on(".nav-link", new NavLinkActiveHandler("blog"))
       .on("title", new TextHandler(metaTitle))
       .on('meta[name="description"]', new AttributeHandler("content", metaDesc))
       .on(
@@ -282,6 +281,7 @@ async function handleBlogPost(slug, request, env) {
           }
         },
       })
+      .on("body", new BlogNavFixScript())
       .transform(templateRes);
   } catch (error) {
     return new Response(error.message, { status: 500 });
@@ -790,5 +790,31 @@ class BlogListSchemaHandler {
       },
     };
     element.setInnerContent(JSON.stringify(schema, null, 2), { html: true });
+  }
+}
+
+class BlogNavFixScript {
+  element(element) {
+    element.append(
+      `<script>
+(function(){
+  function fixBlogNav() {
+    document.querySelectorAll('a.nav-link').forEach(function(link) {
+      var href = link.getAttribute('href') || '';
+      if (href === '/blog' || href === '/blog/' || href.indexOf('blog') > -1 && href.indexOf('blog/') === -1) {
+        link.classList.add('w--current');
+        link.setAttribute('aria-current', 'page');
+      }
+    });
+  }
+  if (document.readyState === 'complete') {
+    setTimeout(fixBlogNav, 0);
+  } else {
+    window.addEventListener('load', function() { setTimeout(fixBlogNav, 0); });
+  }
+})();
+</script>`,
+      { html: true },
+    );
   }
 }
