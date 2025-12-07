ece5dd7b853ce6eb90203b1bc4056722af55a5c3
Silso <dev@sil.so>
2025-12-07 03:49:28 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -514,11 +514,11 @@ function convertBlocksToHtml(blocks) {
           : "";
 
         html += `
-            <details class="accordion-item">
+            <details class="accordion-item" data-accordion-start-open="false">
               <summary class="accordion-trigger"><div class="accordion-title h6">${summary}</div><svg class="accordion-icon" xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 32 32" fill="none" class="accordion-icon">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M17 17L27.3137 17L27.3137 15H17V4.68631L15 4.68631L15 15H4.68629L4.68629 17L15 17V27.3137H17V17Z" fill="currentColor"></path>
               </svg></summary>
-              <div class="accordion-content" style="overflow: clip; height: 0px;">
+              <div data-accordion="content" class="accordion-content" style="overflow: clip; height: 0px;">
                 <div class="accordion-content_spacer">
                   ${childrenHtml}
                 </div>
